61f88a139938329f0a2e928f1ad7066c345fec72
wangzhouzhou <wangzhouzhou@dreame.tech>
2024-10-18 16:18:21 +0800

    feat: init

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div>
+      <div>这里是About</div>
+      <Link href="/">点击返回首页</Link>
+    </div>
+  );
+};
+
+export default About;
